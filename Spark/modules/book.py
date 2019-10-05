2a6824d5af34174588bb5890b30a66133712f21e
Thiago Julião <46033397+thiagojuliao@users.noreply.github.com>
2019-10-04 23:13:02 -0300

    Update book.py
    Changelog:
    • Corrigido o erro de "list index out of range" da versão anterior.
    • Implementado a atualização e visualização do Metadados específico para o objeto Book.
    • Conclusão do procedimento de criação de variáveis de 1º Camada.
    • Implementado (porém ainda não testado) utilização de um público voltado para desenvolvimento, ou seja, com mais referências.

--- Spark/modules/book.py
+++ Spark/modules/book.py
@@ -25,9 +25,45 @@ class Book(Dataform):
             pass
     
     
+    # Retorna o Público
+    @property
+    def publico(self):
+      return self.Publico
+    
+    
+    # Método de atualização do metadados (Book)
+    def __updateMetadata__(self, col_list, dict_list):
+
+      # Verifica e insere informações novas
+      for coluna in [nova_coluna for nova_coluna in col_list if nova_coluna not in self.Dataframe.columns]:
+        self.Metadata.append({"nome": coluna, "origem": None, "tipo": "StringType()", "valor_se_nulo": None, "dominio": None, "transformacao": None, 
+                              "filtro": None, "janela_de_tempo": None, "variavel_temporal": None, "agregacao": None, "duplicidade": None, "versao": self.ref})
+
+      # Atualiza de fato o metadados
+      for info in self.Metadata:
+        if info["nome"] in col_list:
+          index = col_list.index(info["nome"])
+          dict = dict_list[index]
+
+          for chave in dict.keys():
+            info[chave] = dict[chave]
+    
+    
+    # Visualização do Metadados (Book)
+    def viewMetadata(self, truncate=True):
+
+      meta_DF = self.__buildMetadataDF__()
+      meta_DF = meta_DF.select(col("nome").alias("Nome"), col("origem").alias("Origem"), col("tipo").alias("Tipo"), col("valor_se_nulo").alias("Valor/Expressão(SQL) se Nulo"), 
+                               col("dominio").alias("Domínio"), col("transformacao").alias("Transformação(SQL)"), col("filtro").alias("Filtro(SQL) na Origem"),
+                               col("janela_de_tempo").alias("(Range de Datas, Unidade de Tempo)"), col("variavel_temporal").alias("Variável Temporal"),
+                               col("agregacao").alias("Função de Agregação"), col("duplicidade").alias("Remoção de Duplicidade por"), col("versao").alias("Versão"))
+      
+      return meta_DF.show(len(self.Metadata), truncate)
+    
+    
     # Realiza extração dos dados
     def __extractor__(self, ref, dict):
-        
+      
         # Aplica filtro(s) na origem caso necessário
         if dict["filtro"]:
           filtrada = self.Origem.select(self.chave_primaria, dict["origem"], dict["variavel_temporal"]).filter(dict["filtro"])
@@ -53,33 +89,46 @@ class Book(Dataform):
         # Verifica e remove duplicidade
         if dict["duplicidade"]:
             extracao = extracao.dropDuplicates(dict["duplicidade"])
-        
+            
         return extracao.withColumn(self.chave_temporal, lit(ref))
     
     
     # Módulo de construção de variáveis
     def bookedAs(self, lst):
+        
         vars_com_origem = [var for var in lst if var["origem"] and var["nome"] not in self.Dataframe.columns]
         vars_sem_origem = [var for var in lst if not var["origem"] and var["nome"] not in self.Dataframe.columns]
         
-        # Etapa 01 - Gera vetor de datas para extração
+        # Etapa 01 - Insere no metadados as informações das variáveis novas
+        variaveis = [var["nome"] for var in lst if var["nome"] not in self.Dataframe.columns]
+        self.__updateMetadata__(variaveis, lst)
+        
+        # Etapa 02 - Gera vetor de datas para extração a partir do público
         vetor_de_datas = [str(data[self.chave_temporal]) for data in self.Publico.select(self.chave_temporal).orderBy(self.chave_temporal).dropDuplicates().collect()]
             
         # Inicia a construção das variáveis que possuem origem
         for info in vars_com_origem:
             
-            aux = vetor_de_datas
+            aux = vetor_de_datas.copy()
             
-            # Etapa 02 - Extração
+            # Etapa 03 - Extração
             extracao = self.__extractor__(aux[0], info)
             aux.remove(aux[0])
             
             for ref in vetor_de_datas:
                 extracao = extracao.union(self.__extractor__(ref, info))
                 
-            # Etapa 03 - Agregação
+            # Etapa 04 - Agregação
             agregacao = extracao.groupBy(self.chave_primaria, self.chave_temporal).agg(expr(info["agregacao"]).alias(info["nome"]))
             
-            # Etapa 04 - Recupera Público
+            # Etapa 05 - Recupera Público
             self.Dataframe = self.Dataframe.join(agregacao, on = [self.chave_primaria, self.chave_temporal], how = "left")
+            
+            # Etapa 06 - Executa os módulos do objeto Dataform
+            self.transformedAs({info["nome"]: info["transformacao"]})
+            self.coalescedAs({info["nome"]: info["valor_se_nulo"]})
+            self.castedAs({info["nome"]: info["tipo"]})
+            self.domainAs({info["nome"]: info["dominio"]})
+            
+            
             
