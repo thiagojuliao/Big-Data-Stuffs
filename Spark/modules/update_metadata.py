22832e988dee1228cd4f609084102cb8c3e075dc
Thiago Julião <46033397+thiagojuliao@users.noreply.github.com>
2019-10-08 17:36:38 -0300

    Update update_metadata.py
    Changelog:
    • Alteração do nome da função para readMetadata().
    • Incorporado tratamento para os campos de janela de tempo utilizados no objeto Book.

--- Spark/modules/update_metadata.py
+++ Spark/modules/update_metadata.py
@@ -1,4 +1,5 @@
-def uploadMetadata(dir):
+def readMetadata(dir):
+  
   import json
   
   # Lendo o metadados do diretório especificado
@@ -7,10 +8,18 @@ def uploadMetadata(dir):
   for linha in spark.sparkContext.textFile(dir).collect():
     info = json.loads(linha)
 
-    # Faz o tratamento dos campos com conteúdo 'None'
+    # Faz o tratamento dos campos com conteúdo 'None' e monta o array da janela de tempo
     for chave in info:
       if info[chave] == "None":
         info[chave] = None
+        
+      if chave == "janela_de_tempo" and info[chave]:
+        string = info[chave]
+        range00 = int(string.split("([")[1].split(",")[0])
+        range01 = int(string.split("([")[1].split(",")[1].split("]")[0])
+        unidade = string.split("([")[1].split(",")[2].split(")")[0].strip().replace("'", "")
+        info[chave] = [(range00, range01), unidade]
+        
     metadados.append(info)
   
   return metadados
